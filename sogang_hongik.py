974458bc3335ea9ad1c68507a271bdc52d8126ef
2jungwon <james66won@gmail.com>
2019-05-06 06:24:04 +0000

    서강대학교, 홍익대학교 ISBN으로 검색 (a 태그로 검색하여 상세페이지 이동)

--- sogang_hongik.py
+++ sogang_hongik.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from parse import *
 
 def GetURL1(isbn, school):
 	if school==1:
@@ -8,22 +7,22 @@ def GetURL1(isbn, school):
 	elif school==2:
 		return 'http://honors.hongik.ac.kr/search/tot/result?st=EXCT&si=6&q='+isbn
 
-def GetURL2(bookid, school):
+def GetURL2(detail, school):
 	if school==1:
-		return 'http://library.sogang.ac.kr/search/detail/'+bookid
+		return 'http://library.sogang.ac.kr'+detail
 	elif school==2:
-		return 'http://honors.hongik.ac.kr/search/detail/'+bookid
+		return 'http://honors.hongik.ac.kr'+detail
 
 def isValid(url):
 	resp=requests.get(url)
 	html=resp.text
 	soup=BeautifulSoup(html, 'html.parser')
-	bookid=soup.find_all("span", "bookimg")
-	check=str(bookid)
+	detail=soup.find_all("dd", "searchTitle")
+	check=str(detail)
 	if check == '[]':
 		return (False, None)
 	else:
-		return (True, bookid)
+		return (True, detail)
 
 def FindInfo(url):	
 	resp=requests.get(url)
@@ -57,10 +56,9 @@ def main():
 		print('해당 도서가 도서관에 있지 않습니다.')
 		return;
 	else:
-		bookid=r[1]
-		bookid=str(bookid[0].get('id'))
-		bookid=parse("bookImg_{}", bookid)[0]
-		url=GetURL2(bookid, school)
+		detail=r[1]
+		detail=detail[0].find_all("a")[0].get('href')
+		url=GetURL2(detail, school)
 		info=FindInfo(url)
 		for book in info:
 			print(book)
