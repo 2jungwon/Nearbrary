ac5ee7262dd22a7e7f8a0e1ed3e5888661e83ac6
desewenkdk <171018since400@gmail.com>
2019-05-06 15:38:37 +0900

    aaaaaa

--- sogang_hongik.py
+++ sogang_hongik.py
@@ -2,17 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 from parse import *
 
+# 1차 크롤링 - isbn값을 통해, controlno값 : CAT뭐시기저시기를 받아온다.
 def GetURL1(isbn, school):
 	if school==1:
-		return 'http://library.sogang.ac.kr/search/tot/result?st=EXCT&si=6&q='+isbn
+        return 'http://library.sogang.ac.kr/search/tot/result?st=EXCT&si=6&q='+isbn
 	elif school==2:
-		return 'http://honors.hongik.ac.kr/search/tot/result?st=EXCT&si=6&q='+isbn
-
+        return 'http://honors.hongik.ac.kr/search/tot/result?st=EXCT&si=6&q='+isbn
+    elif school==4:
+            return 'https://library.yonsei.ac.kr/main/searchBrief?q='+isbn
+#2차 크롤링 - 1차 크롤링을 통해 받아온 값을 이용해서 대출 가능 여부 정보를 가져온다.
 def GetURL2(bookid, school):
 	if school==1:
 		return 'http://library.sogang.ac.kr/search/detail/'+bookid
 	elif school==2:
 		return 'http://honors.hongik.ac.kr/search/detail/'+bookid
+
 
 def isValid(url):
 	resp=requests.get(url)
@@ -49,21 +53,29 @@ def FindInfo(url):
 	return info
 
 def main():
-	school=int(input('1. 서강대학교, 2. 홍익대학교 : '))
+    school=int(input('1. 서강대학교, 2. 홍익대학교, 4.연세대학교 : '))
 	isbn=input('ISBN : ')
 	url=GetURL1(isbn, school)
-	r=isValid(url)
-	if r[0]==False:
-		print('해당 도서가 도서관에 있지 않습니다.')
-		return;
-	else:
-		bookid=r[1]
-		bookid=str(bookid[0].get('id'))
-		bookid=parse("bookImg_{}", bookid)[0]
-		url=GetURL2(bookid, school)
-		info=FindInfo(url)
-		for book in info:
-			print(book)
+    if school <= 3:
+        r=isValid(url)
+        if r[0]==False:
+            print('해당 도서가 도서관에 있지 않습니다.')
+            return;
+        else:
+            bookid=r[1]
+            bookid=str(bookid[0].get('id'))
+            bookid=parse("bookImg_{}", bookid)[0]
+            url=GetURL2(bookid, school)
+            info=FindInfo(url)
+            for book in info:
+                print(book)
+    else:
+        resp = request.get(url)
+        html = resp.text
+        soup = BeautifulSoup(html, 'lxml')
 
+        get_bookSection = soup.find('div',{'class' : 'bookSection'})
+        print(get_bookSection)
 if __name__ == '__main__':
 	main()
+
